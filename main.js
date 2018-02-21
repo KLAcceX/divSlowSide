ba1ef76ecadcae9cc27365fa605b9a64215b509b
KLAcceX <mrrj12@gmail.com>
2018-02-21 11:03:41 -0300

    Changes 2018 02 21

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const jumpMax = 4;
 const divChar = 'z';
 const LEFT = 0, UP = 1, RIGHT = 2, DOWN = 3;
 var lKey = false, rKey = false;
+var startId;
 
 $(document).on('click', '#main', function(event){
 	if($('#file').val() == null || $('#file').val() == ''){
@@ -19,6 +20,7 @@ $(document).on('change', '#file', function(event){
         	var json = JSON.parse(reader.result);
         	$('#main').append(generateMap(json));
         	interval = setInterval(function() {
+        		conditions();
         		movements();
         	}, delay);
         }
@@ -60,13 +62,11 @@ $(document).on('keyup', function(event){
 			lKey = false;
 			event.preventDefault();
 			break;
-		case 38:
-			event.preventDefault();
-			break;
 		case 39:
 			rKey = false;
 			event.preventDefault();
 			break;
+		case 38:
 		case 40:
 			event.preventDefault();
 			break;
@@ -98,15 +98,20 @@ function generateMap(json){
 			if((num & 32) != 0){
 				div.addClass('rod');
 				var flag = elements.filter('div#' + (parseInt(i) - 1) + divChar + j);
+				flag.addClass('flag');
 				for(var k = 0; k < 50; k++){
 					var square = $('<div/>', {
-						class: 'flag'
+						class: 'square'
 					});
+					if(k < 20){
+						console.log('t added');
+						square.addClass('t');
+					}
 					flag.append(square);
 				}
 			}
 			if((num & 64) != 0){
-				div.addClass('bottom');
+				div.addClass('trap');
 
 				for(var k = 0; k < 6; k++){
 					var space = $('<div/>', {
@@ -123,6 +128,7 @@ function generateMap(json){
 			}
 			if((num & 128) != 0){
 				div.addClass('player');
+				startId = i + divChar + j;
 			}
 			div.attr('id', i + divChar + j);
 			elements = elements.add(div);
@@ -149,53 +155,77 @@ function movements(){
 	}
 }
 
+function conditions(){
+	var player = $('.player').first();
+	if(player.hasClass('trap')){
+		alert("YOU'RE DEAD!");
+		resetPlayer(player);
+		return false;
+	} else if(player.hasClass('rod') || player.hasClass('flag')){
+		alert("Y0U'R3 TH3 B35T!");
+		resetPlayer(player);
+		return false;
+	}
+
+	return true;
+}
+
+function resetPlayer(){
+	$('.player').removeClass('player');
+	lKey = false;
+	rKey = false;
+	jump = false;
+	$('#'+startId).addClass('player');
+}
+
 function move(direction){
 	var player = $('.player').first();
 	if(player.length > 0){
 
 		var id = player.attr('id');
 		var local = id.split(divChar);
+		if(conditions()){
+			switch(direction){
+				case LEFT:
+					if(!player.hasClass('left')){
+						$('#' + id).removeClass('player');
+						$('#' + local[0] + divChar + (parseInt(local[1]) - 1)).addClass('player');
+					};
+					break;
+				case UP:
+					if(!player.hasClass('top')){
+						if((player.hasClass('bottom') && contador == 0) || (contador < jumpMax && contador > 0)){
+							contador++;
+						}else{
+							jump = false;
+						}
 
-		switch(direction){
-			case LEFT:
-				if(!player.hasClass('left')){
-					$('#' + id).removeClass('player');
-					$('#' + local[0] + divChar + (parseInt(local[1]) - 1)).addClass('player');
-				};
-				break;
-			case UP:
-				if(!player.hasClass('top')){
-					if((player.hasClass('bottom') && contador == 0) || (contador < jumpMax && contador > 0)){
-						contador++;
+						if(jump){
+							$('#' + id).removeClass('player');
+							$('#' + (parseInt(local[0]) - 1) + divChar + local[1]).addClass('player');
+						}
 					}else{
 						jump = false;
 					}
-
-					if(jump){
+					break;
+				case RIGHT:
+					if(!player.hasClass('right')){
 						$('#' + id).removeClass('player');
-						$('#' + (parseInt(local[0]) - 1) + divChar + local[1]).addClass('player');
+						$('#' + local[0] + divChar + (parseInt(local[1]) + 1)).addClass('player');
 					}
-				}else{
-					jump = false;
-				}
-				break;
-			case RIGHT:
-				if(!player.hasClass('right')){
-					$('#' + id).removeClass('player');
-					$('#' + local[0] + divChar + (parseInt(local[1]) + 1)).addClass('player');
-				}
-				break;
-			case DOWN:
-				if(player.hasClass('bottom')){
-					contador = 0;
-				}else if(contador > 0){
-					contador--;
-				}
-				if(!player.hasClass('bottom')){
-					$('#' + id).removeClass('player');
-					$('#' + (parseInt(local[0]) + 1) + divChar + local[1]).addClass('player');
-				}
-				break;
+					break;
+				case DOWN:
+					if(player.hasClass('bottom')){
+						contador = 0;
+					}else if(contador > 0){
+						contador--;
+					}
+					if(!player.hasClass('bottom')){
+						$('#' + id).removeClass('player');
+						$('#' + (parseInt(local[0]) + 1) + divChar + local[1]).addClass('player');
+					}
+					break;
+			}
 		}
 	}
 
